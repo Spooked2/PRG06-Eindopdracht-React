02c2c2cb6f6ad00d85a773357cd2549e10f90a00
Sander <sanderlandm@gmail.com>
2025-03-23 14:52:57 +0100

    ADD home page now has a little styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
+import {Link} from "react-router";
+import './Home.css';
 
 function Home() {
 
     return (
-        <section className={"solidBackground"}>
+        <section>
             <h1>Court record</h1>
+
+            <div className={'homeButtons'}>
+                <Link to={'/games'}>Games</Link>
+                <Link to={'/cases'}>Cases</Link>
+                <Link to={'/evidence'}>Evidence</Link>
+                <Link to={'/profiles'}>Profiles</Link>
+            </div>
         </section>
     );
 }
